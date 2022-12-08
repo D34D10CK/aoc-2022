be6176ca6609b26a8deefe3fdb43322c2aad6471
D34D10CK <leonard.berney@bluewin.ch>
2022-12-08 01:40:29 +0100

    simplify day 7

--- day7/day7.fsx
+++ day7/day7.fsx
@@ -1,26 +1,6 @@
 open System.IO
 open System.Text.RegularExpressions
 
-type Directory =
-    { Parent: Directory Option
-      mutable Children: Directory list
-      mutable Size: int
-      Name: string }
-
-let rec flatten dir =
-    match dir.Children with
-    | [] -> [ dir ]
-    | x -> dir :: (x |> List.map flatten |> List.concat)
-
-let rec sizeOf dir =
-    match dir.Children with
-    | [] -> dir.Size
-    | x ->
-        List.sum
-            [ for c in x do
-                  sizeOf c ]
-        + dir.Size
-
 let (|ParseRegex|_|) regex str =
     let m = Regex(regex).Match(str)
 
@@ -29,34 +9,26 @@ let (|ParseRegex|_|) regex str =
     else
         None
 
-let parseLine pwd line =
+let parseLine ((fs: Map<string list, int>), (currPath: string list)) (line: string) =
     match line with
     | ParseRegex @"^\$ cd (\w+)$" [ dir ] ->
-        let newDir =
-            { Parent = Some(pwd)
-              Children = []
-              Size = 0
-              Name = dir }
-
-        pwd.Children <- newDir :: pwd.Children
-        newDir
-    | "$ cd .." -> pwd.Parent.Value
-    | ParseRegex @"^(\d+).*" [ size ] ->
-        pwd.Size <- pwd.Size + int size
-        pwd
-    | _ -> pwd
+        let newPath = dir :: currPath
+        fs.Add(newPath, 0), newPath
+    | "$ cd .." -> fs, currPath.Tail
+    | ParseRegex @"^(\d+).*" [ size ] -> fs.Change(currPath, (fun x -> Some(int size + x.Value))), currPath
+    | _ -> fs, currPath
 
 let lines = File.ReadAllLines("input.txt") |> Array.toList
 
-let rootDir =
-    { Parent = None
-      Children = []
-      Size = 0
-      Name = "/" }
+let fs =
+    ((Map [ ([ "/" ], 0) ], [ "/" ]), lines.Tail) ||> List.fold parseLine |> fst
 
-(rootDir, lines.Tail) ||> List.fold parseLine
-let flat = flatten rootDir
-let sizes = flat |> List.map sizeOf
+let fsList =
+    fs |> Map.toList |> List.map (fun x -> fst x |> String.concat "/", snd x)
+
+let sizes =
+    fsList
+    |> List.map (fun (x1, _) -> fsList |> List.filter (fun (x2, _) -> x2.EndsWith(x1)) |> List.sumBy snd)
 
 let part1 = sizes |> List.filter (fun x -> x <= 100000) |> List.sum
 printfn $"Question 1: {part1}"
