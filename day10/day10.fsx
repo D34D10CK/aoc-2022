5be91454fb93f513b27f5d368924ac1706d86f52
D34D10CK <leonard.berney@bluewin.ch>
2022-12-11 01:27:30 +0100

    day 10

--- day10/day10.fsx
+++ day10/day10.fsx
@@ -13,15 +13,32 @@ let parseLine stack (line: string) =
 
     stack @ cycles
 
+let drawPixel (stack: int list) cycle =
+    let spriteLeft = stack[cycle] - 1
+    let spriteRight = stack[cycle] + 1
+    let position = cycle % 40
+
+    if position >= spriteLeft && position <= spriteRight then
+        '#'
+    else
+        '.'
+
 let lines = File.ReadAllLines("input.txt")
 let stack = ([ 1 ], lines) ||> Array.fold parseLine
 
-let sum1 =
-    stack[19] * 20
-    + stack[59] * 60
-    + stack[99] * 100
-    + stack[139] * 140
-    + stack[179] * 180
-    + stack[219] * 220
+let part1 =
+    [ for i in [ 20..40..220 ] do
+          stack[i - 1] * i ]
+    |> List.sum
 
-printfn $"Question 1: {sum1}"
+printfn $"Question 1: {part1}"
+
+let crt = [| 0..239 |] |> Array.map (fun x -> drawPixel stack x) |> System.String
+
+let crt2d =
+    [ for i in [ 0..40..200 ] do
+          crt[i .. i + 39] ]
+    |> String.concat "\n"
+
+printfn "Question 2:"
+printfn $"{crt2d}"
